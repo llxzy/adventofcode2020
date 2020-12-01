a037d1fc2c9e26ec7ad51fcb8e6de8211d0dd64e
llxzy <snowcrash@tuta.io>
2020-12-01 19:42:06 +0100

    added rust implementation of day1

---
+++ rust/target/debug/deps/rust-62fe48523ca6f14c.d
@@ -0,0 +1,6 @@
+/home/nnn/programming/adventofcode2020/rust/target/debug/deps/rust-62fe48523ca6f14c: src/main.rs src/day1.rs
+
+/home/nnn/programming/adventofcode2020/rust/target/debug/deps/rust-62fe48523ca6f14c.d: src/main.rs src/day1.rs
+
+src/main.rs:
+src/day1.rs:
---
+++ rust/target/rls/debug/deps/rust-f41917cd8004e782.d
@@ -0,0 +1,6 @@
+/home/nnn/programming/adventofcode2020/rust/target/rls/debug/deps/rust-f41917cd8004e782.rmeta: src/main.rs src/day1.rs
+
+/home/nnn/programming/adventofcode2020/rust/target/rls/debug/deps/rust-f41917cd8004e782.d: src/main.rs src/day1.rs
+
+src/main.rs:
+src/day1.rs:
